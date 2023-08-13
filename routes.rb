ba9c1f249df9890bbeb73f09b234aef175266cdc
EC2 Default User <ec2-user@ip-172-31-24-98.ap-northeast-1.compute.internal>
2023-08-13 18:31:03 +0000

    2nd_No5

---
+++ routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # PostsController
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  get 'topics/new', to: 'topics#new', as: 'new_topics'
+  get 'topics/edit', to: 'topics#edit', as: 'edit_topics'
+end
+
+
